ba2cbb8b02dbf9b9bd6908d9b42225677e703e65
Sankruthya-Krishna <sankruthyakrishna13@gmail.com>
2024-03-01 14:59:30 +0530

    step-17...dialogue fragment

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,10 +1,34 @@
-# App Descriptor
-appTitle=Hello World
-appDescription=A simple walkthrough app that explains the most important concepts of SAPUI5
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], (Controller, MessageToast) => {
+	"use strict";
 
-# Hello Panel
-showHelloButtonText=Say Hello
-helloMsg=Hello {0}
-homePageTitle=Walkthrough
-helloPanelTitle=Hello World
-openDialogButtonText=Say Hello With Dialog
+	return Controller.extend("ui5.walkthrough.controller.HelloPanel", {
+		onShowHello() {
+			// read msg from i18n model
+			const oBundle = this.getView().getModel("i18n").getResourceBundle();
+			const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+			const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+			// show message
+			MessageToast.show(sMsg);
+		},
+
+		onOpenDialog() {
+			// create dialog lazily
+			this.pDialog ??= this.loadFragment({
+				name: "ui5.walkthrough.view.HelloDialog"
+			});
+
+			this.pDialog.then((oDialog) => oDialog.open());
+		},
+
+		onCloseDialog() {
+			// note: We don't need to chain to the pDialog promise, since this event handler
+			// is only called from within the loaded dialog itself.
+			this.byId("helloDialog").close();
+		}
+	});
+
+});
