f2df845f5c3433914cc5311d4232f9899cd21d66
Sankruthya-Krishna <sankruthyakrishna13@gmail.com>
2024-03-04 11:13:37 +0530

    custom formatting

---
+++ webapp/controller/InvoiceList.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"../model/formatter"
+], (Controller, JSONModel,formatter) => {
+	"use strict";
+
+	return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
+		formatter: formatter,
+		onInit() {
+			const oViewModel = new JSONModel({
+				currency: "IND"
+			});
+			this.getView().setModel(oViewModel, "view");
+		}
+	});
+});
